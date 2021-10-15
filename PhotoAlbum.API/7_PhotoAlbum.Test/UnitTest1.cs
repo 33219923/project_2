7e9b9a01d7a664db05fea403e56ef101a91b4cad
Arno Brand <acnolgen@gmail.com>
2021-10-15 11:38:53 +0200

    Added default unit test project

---
+++ PhotoAlbum.API/7_PhotoAlbum.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _7_PhotoAlbum.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
