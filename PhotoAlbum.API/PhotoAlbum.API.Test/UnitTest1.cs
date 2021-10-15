013bbdfa79dac18772add4a1cfcf6f0eb0ac4336
Arno Brand <acnolgen@gmail.com>
2021-10-15 11:46:22 +0200

    Renamed projects and added project dependencies

---
+++ PhotoAlbum.API/PhotoAlbum.API.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PhotoAlbum.API.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
