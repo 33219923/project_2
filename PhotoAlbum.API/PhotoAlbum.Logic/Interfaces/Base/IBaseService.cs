edaa229cda59ed9c46209917d724d6cd2efddfd5
Arno Brand <acnolgen@gmail.com>
2021-11-06 02:57:18 +0200

    Added base service methods

--- PhotoAlbum.API/PhotoAlbum.Logic/Interfaces/Base/IBaseService.cs
+++ PhotoAlbum.API/PhotoAlbum.Logic/Interfaces/Base/IBaseService.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace PhotoAlbum.Logic.Interfaces.Base
 {
-    public interface IBaseService
+    public interface IBaseService<T>
     {
+        T Get(Guid id);
+        List<T> ListAll();
+        T Add(T dto);
+        T Update(T dto, Guid id);
+        void Delete(Guid id);
     }
 }
